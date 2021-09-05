662c97b4b126e1599a977d86ae39f251dcb021b5
Spenser <spenser@minddnd.com>
2021-09-05 14:46:26 -0400

    assets

---
+++ src/routes/__layout.svelte
@@ -0,0 +1,5 @@
+<script>
+	import '$lib/global.scss'
+</script>
+
+<slot />
--- svelte.config.js
+++ svelte.config.js
@@ -1,9 +1,10 @@
-/** @type {import('@sveltejs/kit').Config} */
+import preprocess from 'svelte-preprocess'
 const config = {
 	kit: {
 		// hydrate the <div id="svelte"> element in src/app.html
 		target: '#svelte'
-	}
+	},
+	preprocess: preprocess()
 };
 
 export default config;
