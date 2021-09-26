c452cd42e118f30916de4edb8417a749b84385ee
Spenser <spenser@minddnd.com>
2021-09-26 00:56:13 -0400

    more card info

--- src/lib/components/CritterCard.svelte
+++ src/lib/components/CritterCard.svelte
@@ -20,6 +20,14 @@
 		}
 	}
 
+	function timeConvert(time) {
+		if(time === 0){
+			return `12am`
+		}else{
+			return time > 12 ? `${time - 12}pm` : `${time}am`
+		}
+	}
+
 	$: flickPrice = critter.price * 1.5
 </script>
 
@@ -32,11 +40,11 @@
 	</section>
 
 	<div class="grid col-2 gap-none">
-		<div class="price">
+		<div class="price info-block">
 			<span class="label">Price</span>
 			<p>${ Intl.NumberFormat('en-US').format(critter.price) }</p>
 		</div>
-		<div class="price flick">
+		<div class="price flick info-block">
 			<span class="label">Flick's Price</span>
 			<p>${ Intl.NumberFormat('en-US').format(flickPrice) }</p>
 		</div>
@@ -45,6 +53,56 @@
 	<blockquote class="phrase">
 		"{ critter.phrase }"
 	</blockquote>
+
+	<div class="grid {dir === 'fish' ? 'col-3' : 'col-1'}">
+		{#if dir === 'fish'}
+			<div class="size info-block">
+				<span class="label">Size</span>
+				<p>{ critter.size }</p>
+			</div>
+		{/if}
+		
+		<div class="location info-block">
+			<span class="label">Location</span>
+			<p>{ critter.location }</p>
+		</div>
+	</div>
+
+	<div class="info-block time">
+		<span class="label">Time</span>
+		{#if Array.isArray(critter.start)}
+			{#each critter.start as start, i}
+				<p>{ timeConvert(start) } - { timeConvert(critter.end[i]) }</p>
+			{/each}
+		{:else if critter.start === 0 && critter.end === 24}
+			<p>All Day</p>
+		{:else}
+			<p>{ timeConvert(critter.start) } - { timeConvert(critter.end) }</p>
+		{/if}
+	</div>
+
+	<div class="info-block months">
+		<span class="label">Months</span>
+		<div class="grid col-4 gap-1">
+			<p class="month" class:active={critter.months.includes('jan')}>Jan</p>
+			<p class="month" class:active={critter.months.includes('feb')}>Feb</p>
+			<p class="month" class:active={critter.months.includes('mar')}>Mar</p>
+			<p class="month" class:active={critter.months.includes('apr')}>Apr</p>
+			<p class="month" class:active={critter.months.includes('may')}>May</p>
+			<p class="month" class:active={critter.months.includes('jun')}>Jun</p>
+			<p class="month" class:active={critter.months.includes('jul')}>Jul</p>
+			<p class="month" class:active={critter.months.includes('aug')}>Aug</p>
+			<p class="month" class:active={critter.months.includes('sept')}>Sept</p>
+			<p class="month" class:active={critter.months.includes('oct')}>Oct</p>
+			<p class="month" class:active={critter.months.includes('nov')}>Nov</p>
+			<p class="month" class:active={critter.months.includes('dec')}>Dec</p>
+		</div>
+	</div>
+
+	<div class="rarity info-block {critter.rarity}">
+		<span class="label">Rarity</span>
+		<p>{ critter.rarity }</p>
+	</div>
 
 </main>
 
@@ -93,12 +151,14 @@
 	span.label{
 		display: inline-block;
 		background: var(--gold);
-		color: var(--brown);
+		color: var(--dbrown);
 		text-align: center;
 		border-radius: 100px;
 		padding: 5px 10px;
 		font-size: 1.2rem;
 		margin-bottom: 5px;
+		box-shadow: 2px 2px 10px rgb(0 0 0 / 20%);
+		border: solid 1px var(--dbrown)
 	}
 
 	.icon{
@@ -164,23 +224,19 @@
 
 	blockquote{
 		font-style: italic;
-		color: var(--color);
+		color: var(--dark);
 		font-weight: normal;
 		text-shadow: 1px 1px 2px hsl(420 69% 0% / 25%);
 	}
 
-	.price {
-		background-color: var(--bg);
+	.info-block{
 		background-image: url('/play-dots.png');
 		background-size: var(--dot-size);
 		padding: 2rem 1rem 2rem;
 		color: var(--light);
 		text-align: center;
+		text-transform: capitalize;
 
-		&.flick {
-			background-color: var(--color);
-		}
-		
 
 		p{
 			font-size: 3.2rem;
@@ -188,7 +244,52 @@
 			text-align: center;
 			text-shadow: 1px 1px 0 #77770c;
 		}
+	}
 
+	.price {
+		background-color: var(--bg);
+
+		&.flick {
+			background-color: var(--color);
+		}
+
+	}
+
+	.location{
+		background-color: var(--dbrown);
+	}
+
+	.time > p{
+		color: var(--dark);
+		text-shadow: 1px 1px 2px rgb(0 0 0 /25%);
+	}
+
+
+	p.month{
+		font-style: italic;
+		border: solid 2px var(--brown);
+		background: transparent;
+		color: var(--dbrown);
+		border-radius: 10rem;
+		font-size: 1.6em;
+		padding: 1rem;
+		opacity: .5;
+
+		&.active{
+			opacity: 1;
+			background: var(--gold);
+			color: var(--brown);
+			text-shadow: none;
+		}
+	}
+
+	.rarity{
+		background-color: var(--brown);
+
+		&.common{ background-color: var(--lgreen); }
+		&.uncommon{ background-color: var(--blue); }
+		&.rare{ background-color: var(--purple); }
+		&.ultra-rare{ background-color: var(--gold); }
 	}
 
 
