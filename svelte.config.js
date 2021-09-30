31363604b8af752174ceab5092e1975ebf18ca7e
Spenser <spenser@minddnd.com>
2021-09-30 12:20:12 -0400

    lets give this a try

--- svelte.config.js
+++ svelte.config.js
@@ -7,15 +7,8 @@ const config = {
 		// hydrate the <div id="svelte"> element in src/app.html
 		target: '#svelte',
 		adapter: adapter(),
-		vite: {
-			define: {
-				'process.env.VITE_BUILD_TIME': JSON.stringify(new Date().toISOString())
-			},
-			// plugins: [imagetools({ force: true })],
-			// assetsInclude: ['static/**/*']
-		}
 	},
 	preprocess: [preprocess()]
-};
+}
 
 export default config;
