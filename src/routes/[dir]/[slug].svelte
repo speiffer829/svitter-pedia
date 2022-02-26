1a3191ad3c786f58ae4d026cfb2c7cee274f3623
Spenser <spenser@minddnd.com>
2022-02-25 20:59:12 -0500

    rework of slug route

--- src/routes/[dir]/[slug].svelte
+++ src/routes/[dir]/[slug].svelte
@@ -1,118 +1,116 @@
 <script context="module">
 	export async function load({ params, fetch }) {
-		const { dir, slug } = params
+		const { dir, slug } = params;
 
-		const req = await fetch(`/api/${dir}/${slug}.json`)
-		const res = await req.json()
-		const { critter } = res
+		const req = await fetch(`/api/${dir}/${slug}.json`);
+		const res = await req.json();
+		const { critter } = res;
 
-		return{
+		return {
 			props: {
 				dir,
-				critter
-			}
-		}
+				critter,
+			},
+		};
 	}
 </script>
 
 <script>
-	import CritterImg from '$lib/components/CritterImg.svelte'
-	import CritterCard from '$lib/components/CritterCard.svelte'
+	import CritterImg from '$lib/components/CritterImg.svelte';
+	import CritterCard from '$lib/components/CritterCard.svelte';
 	export let critter;
 	export let dir;
 
-
 	const findThemeColor = () => {
-		if(critter.price >= 10000){
+		if (critter.price >= 10000) {
 			return 'hsl(52.4, 77.5%, 72%)';
-		}else if(critter.price >= 5000){
+		} else if (critter.price >= 5000) {
 			return 'hsl(271.8, 25.6%, 71%)';
-		}else if(critter.price >= 1000){
+		} else if (critter.price >= 1000) {
 			return 'hsl(211.4, 51.2%, 64%)';
-		}else{
+		} else {
 			return 'hsl(161.4, 49.9%, 67.1%)';
 		}
-	}
+	};
 
-	let themeColor = findThemeColor()
-
-
+	let themeColor = findThemeColor();
 
 	const findMoneyBracket = () => {
-		if(critter.price >= 10000){
+		if (critter.price >= 10000) {
 			return 'gold';
-		}else if(critter.price >= 5000){
+		} else if (critter.price >= 5000) {
 			return 'purple';
-		}else if(critter.price >= 1000){
+		} else if (critter.price >= 1000) {
 			return 'blue';
-		}else{
+		} else {
 			return 'green';
 		}
-	}
+	};
 
-	let moneyBracket = findMoneyBracket()
+	let moneyBracket = findMoneyBracket();
 
-	$: width = dir === 'fish' ? 1024 : 600
-	$: height = dir === 'fish' ? 512 : 600
+	$: width = dir === 'fish' ? 1024 : 600;
+	$: height = dir === 'fish' ? 512 : 600;
 </script>
 
 <svelte:head>
-	<title>{critter.name} | Critterpoo</title>
-	<meta name="theme-color" 
-      content={themeColor}>
+	<title>{critter.name} | Critterpedia</title>
+	<meta name="theme-color" content={themeColor} />
 </svelte:head>
 
-
-	<div class="body-bg {moneyBracket}">
-		<div class="grid col-2-md gap-3-md">
-			<CritterImg src={`/${dir}-detailed/${critter.detailedImg}`} alt={critter.name} {width} {height} />
-			<CritterCard {critter} {moneyBracket} {dir} />
-		</div>
+<div class="body-bg {moneyBracket}">
+	<div class="grid col-2-md gap-3-md">
+		<CritterImg
+			src={`/${dir}-detailed/${critter.detailedImg}`}
+			alt={critter.name}
+			{width}
+			{height}
+		/>
+		<CritterCard {critter} {moneyBracket} {dir} />
 	</div>
+</div>
 
+<style lang="scss">
+	div.body-bg {
+		width: 100%;
+		min-height: 100vh;
+		background-color: var(--bg, var(--tan));
+		background-image: var(--bgImage, url('/leaf-test.png'));
+		background-attachment: fixed;
 
-	<style lang="scss">
-
-		div.body-bg{
-			width: 100%;
-			min-height: 100vh;
-			background-color: var(--bg, var(--tan));
-			background-image: var(--bgImage, url('/leaf-test.png'));
-			background-attachment: fixed;
-
-			&.gold{
-				--bg: var(--yellow);
-				--bgImage: url('/gold-leafs.webp');
-			}
-			&.purple{
-				--bg: var(--purple);
-				--bgImage: url('/purple-leafs.webp');
-			}
-			&.blue{
-				--bg: var(--blue);
-				--bgImage: url('/blue-leafs.webp');
-			}
-			&.green{
-				--bg: var(--mint);
-				--bgImage: url('/green-leafs.webp');
-			}
-			&.dark{
-				--bg: var(--dark);
-				--bgImage: url('/play-dots.png');
-			}
-
-			@media screen and (min-width: 767px) {
-				--bg: var(--dark);
-			}
+		&.gold {
+			--bg: var(--yellow);
+			--bgImage: url('/gold-leafs.webp');
 		}
-		div.grid{
-			max-width: 1100px;
-			margin: 0 auto;
-			isolation: isolate;
-			
-			@media screen and (min-width: 767px) {
-				padding: 0rem 2rem;
-				position: relative;
-			}
+		&.purple {
+			--bg: var(--purple);
+			--bgImage: url('/purple-leafs.webp');
 		}
-	</style>
+		&.blue {
+			--bg: var(--blue);
+			--bgImage: url('/blue-leafs.webp');
+		}
+		&.green {
+			--bg: var(--mint);
+			--bgImage: url('/green-leafs.webp');
+		}
+		&.dark {
+			--bg: var(--dark);
+			--bgImage: url('/play-dots.png');
+		}
+
+		@media screen and (min-width: 767px) {
+			--bg: var(--dark);
+		}
+	}
+	div.grid {
+		max-width: 1100px;
+		margin: 0 auto;
+		isolation: isolate;
+
+		@media screen and (min-width: 767px) {
+			padding: 0rem 2rem;
+			position: relative;
+		}
+	}
+</style>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,7 +1,14 @@
 <script context="module">
-	export async function load({fetch}) {
-		const req = await fetch('/api/homepage.json')
-		const { leavingArray, newThisMonthArray, comingNextMonthArray, mostValuableNow  } = await req.json()
+	export async function load({ fetch }) {
+		const req = await fetch('/api/homepage.json');
+		const {
+			leavingArray,
+			newThisMonthArray,
+			comingNextMonthArray,
+			mostValuableNow,
+		} = await req.json();
+
+		const obj = { title: 'foo', lip: 'taste', leg: 'foot ', flim: 'flam', butt: 'hole' };
 
 		return {
 			props: {
@@ -9,30 +16,28 @@
 				newThisMonthArray,
 				comingNextMonthArray,
 				mostValuableNow,
-			}
-		}
+			},
+		};
 	}
 </script>
 
 <script>
-	
 	export let leavingArray, newThisMonthArray, comingNextMonthArray, mostValuableNow;
 </script>
-
 
 <h1>Welcome To My Critterpedia!</h1>
 
 <section class="grid col-2-md gap-2">
 	{#if newThisMonthArray.length}
-	<article>
-		<h2>New This Month</h2>
-		{#each newThisMonthArray as critter}
-			<a href={`/${critter.type}/${critter.slug}`}>
-				<img src={`/${critter.type}/${critter.img}.webp`} alt={critter.name}>
-				<span>{ critter.name }</span>
-			</a>
-		{/each}
-	</article>
+		<article>
+			<h2>New This Month</h2>
+			{#each newThisMonthArray as critter}
+				<a href={`/${critter.type}/${critter.slug}`}>
+					<img src={`/${critter.type}/${critter.img}.webp`} alt={critter.name} />
+					<span>{critter.name}</span>
+				</a>
+			{/each}
+		</article>
 	{/if}
 
 	{#if comingNextMonthArray.length}
@@ -40,8 +45,8 @@
 			<h2>Coming Next Month</h2>
 			{#each comingNextMonthArray as critter}
 				<a href={`/${critter.type}/${critter.slug}`}>
-					<img src={`/${critter.type}/${critter.img}.webp`} alt={critter.name}>
-					<span>{ critter.name }</span>
+					<img src={`/${critter.type}/${critter.img}.webp`} alt={critter.name} />
+					<span>{critter.name}</span>
 				</a>
 			{/each}
 		</article>
@@ -52,8 +57,8 @@
 			<h2>Gone Next Month</h2>
 			{#each leavingArray as critter}
 				<a href={`/${critter.type}/${critter.slug}`}>
-					<img src={`/${critter.type}/${critter.img}.webp`} alt={critter.name}>
-					<span>{ critter.name }</span>
+					<img src={`/${critter.type}/${critter.img}.webp`} alt={critter.name} />
+					<span>{critter.name}</span>
 				</a>
 			{/each}
 		</article>
@@ -64,8 +69,8 @@
 			<h2>Most Valuable Out Right Now</h2>
 			{#each mostValuableNow as critter}
 				<a href={`/${critter.type}/${critter.slug}`}>
-					<img src={`/${critter.type}/${critter.img}.webp`} alt={critter.name}>
-					<span>{ critter.name }</span>
+					<img src={`/${critter.type}/${critter.img}.webp`} alt={critter.name} />
+					<span>{critter.name}</span>
 				</a>
 			{/each}
 		</article>
@@ -73,11 +78,10 @@
 </section>
 
 <style lang="scss">
-	h1{
+	h1 {
 		font-size: 5rem;
 		color: var(--gold);
 		text-align: center;
 		padding-top: 3rem;
 	}
-
 </style>
